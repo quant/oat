9124f42eace72727cd45db8bf97bea06a795793b
Olga Tkachenko <otkach@list.ru>
2013-04-03 09:10:18 +0700

    Fixed indentation

--- greenL.f
+++ greenL.f
@@ -1,6 +1,6 @@
 ! -*- f90 -*-
 ! version %I% of %G%
-!Compute Green's functions 
+!Compute Green's functions
 subroutine calc_GreenL(m,Nx,v,tv,hmag,ef,p,sigmaL,sigmaR,Gnn,Gn1,G1n)
   use params
   implicit none
@@ -22,7 +22,7 @@ subroutine calc_GreenL(m,Nx,v,tv,hmag,ef,p,sigmaL,sigmaR,Gnn,Gn1,G1n)
   complex(WP), allocatable :: gn1old(:,:)
   complex(WP), allocatable :: g1nold(:,:)
   complex(WP), allocatable :: gnnold(:,:)
-!  complex(WP), allocatable :: gnn(:,:,:)
+  !  complex(WP), allocatable :: gnn(:,:,:)
   complex(WP), allocatable :: g1(:,:,:)
   complex(WP), allocatable :: g1new(:,:)
   complex(WP), allocatable :: gmid(:,:)
@@ -33,15 +33,15 @@ subroutine calc_GreenL(m,Nx,v,tv,hmag,ef,p,sigmaL,sigmaR,Gnn,Gn1,G1n)
 
   integer, allocatable :: IPIV(:)
   complex(WP), allocatable :: WORKI(:)
-  
+
   complex(WP) :: emt, emkt, Pij, Pcij, clj
   integer :: k, i, j, n, l
   real(WP) :: eh, err, rr, sled
   integer :: info,LDAI
   integer, save :: LworkI
   complex(WP), parameter::zzero=(0.0,0.0)
-  complex(WP), parameter::zone = (1.0,0.0)   
-! definition  of matrixs for Green recursive functions
+  complex(WP), parameter::zone = (1.0,0.0)
+  ! definition  of matrixs for Green recursive functions
   if (lworki .eq. 0) lworki = 64*M !TODO: lworki as estimate on m
   allocate(Gn1old(1:m,1:m))
   allocate(G1nold(1:m,1:m))
@@ -58,148 +58,148 @@ subroutine calc_GreenL(m,Nx,v,tv,hmag,ef,p,sigmaL,sigmaR,Gnn,Gn1,G1n)
   allocate(IPIV(1:m))
 
   LDAI=m
-! EmHi = E - Hi
+  ! EmHi = E - Hi
   Emhi = (0.0_WP,0.0_WP)
-!  EmHi(1,2) = 1.
-!  do k = 2, M-1
-!     EmHi(k,k-1) = 1.!tv
-!     EmHi(k,k+1) = 1.!tv
-!  end do
-!  EmHi(M,M-1) = 1.!tv
+  !  EmHi(1,2) = 1.
+  !  do k = 2, M-1
+  !     EmHi(k,k-1) = 1.!tv
+  !     EmHi(k,k+1) = 1.!tv
+  !  end do
+  !  EmHi(M,M-1) = 1.!tv
 
-! First we calculate Green's function G1 for every isolated column i=1,2,..,N
+  ! First we calculate Green's function G1 for every isolated column i=1,2,..,N
   do i = 1, Nx
-  Emhi = (0.0_WP,0.0_WP)
+     Emhi = (0.0_WP,0.0_WP)
      do k = 1, M
         EmHi(  k,  k) = -4.0 + ( cmplx(ef,0.000001) - v( i, k ) )/tv
-!        EmHi(  k,  k) = -4.0 + ( cmplx(ef,0.0000001) - v( i, k ) )/tv
+        !        EmHi(  k,  k) = -4.0 + ( cmplx(ef,0.0000001) - v( i, k ) )/tv
      end do
-  EmHi(1,2) = 1.
-  do k = 2, M-1
-     EmHi(k,k-1) = 1.
-     EmHi(k,k+1) = 1.
-  end do
-  EmHi(M,M-1) = 1.
-  g1i=EmHi
-  if(i.eq.1) g1i=g1i-sigmaL
-  if(i.eq.Nx) g1i=g1i-sigmaR
-        call zgetrf( M, M, g1i, LDAI, IPIV, INFO )
-124     call zgetri( M, g1i, LDAI, IPIV, WORKI,LWORKI,INFO )
-        if (info .eq. 0) then 
-          if (real(worki(1)) .gt. lworki) then
-            print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
-            lworki = worki(1) 
-            deallocate(worki)
-            allocate(worki(lworki))
-          endif
-        else if (info .eq. -6) then 
-          print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
-          lworki = worki(1) 
-          deallocate(worki)
+     EmHi(1,2) = 1.
+     do k = 2, M-1
+        EmHi(k,k-1) = 1.
+        EmHi(k,k+1) = 1.
+     end do
+     EmHi(M,M-1) = 1.
+     g1i=EmHi
+     if(i.eq.1) g1i=g1i-sigmaL
+     if(i.eq.Nx) g1i=g1i-sigmaR
+     call zgetrf( M, M, g1i, LDAI, IPIV, INFO )
+124  call zgetri( M, g1i, LDAI, IPIV, WORKI,LWORKI,INFO )
+     if (info .eq. 0) then
+        if (real(worki(1)) .gt. lworki) then
+           print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
+           lworki = worki(1)
+           deallocate(worki)
            allocate(worki(lworki))
-          goto 124
-        else
-          print *, 'cannot continue, info=', info, ' worki(1)=', worki(1)
-          stop
         endif
+     else if (info .eq. -6) then
+        print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
+        lworki = worki(1)
+        deallocate(worki)
+        allocate(worki(lworki))
+        goto 124
+     else
+        print *, 'cannot continue, info=', info, ' worki(1)=', worki(1)
+        stop
+     endif
      do j = 1, M
         do k = 1, M
            G1(  k,  j, i) = g1i( k, j)
         end do
      end do
   end do
-! Then we calculate "left" Green's functions
-! Vn,n+1(l)=t*congj(P(l))=tv*delta_{l,l'}*exp(cmplx(0,2.0*PI*l*hmag,WP))
-! Vn+1,n(l)=t*P(l)=tv*delta_{l,l'}*exp(-cmplx(0,2.0*PI*l*hmag,WP))
+  ! Then we calculate "left" Green's functions
+  ! Vn,n+1(l)=t*congj(P(l))=tv*delta_{l,l'}*exp(cmplx(0,2.0*PI*l*hmag,WP))
+  ! Vn+1,n(l)=t*P(l)=tv*delta_{l,l'}*exp(-cmplx(0,2.0*PI*l*hmag,WP))
   do j = 1, M
      do l = 1, M
         clj  = G1(l,j,1)
-        Gn1(l,j,1) = clj    
+        Gn1(l,j,1) = clj
         G1n(l,j,1) = clj
         Gnn(l,j,1) = clj
         Gnnold(l,j)= clj
         Gn1old(l,j)= clj
         G1nold(l,j)= clj
-!        write(*,*) clj
+        !        write(*,*) clj
      end do
   end do
   do n = 1, Nx-1
 
-    do j = 1, M
-       do l = 1, M
-          clj  = G1(l,j,n+1) ! isolated Green's function of n+1 column
-          G1new(l,j)  = clj
-!        write(*,*) clj
-          Gmid1(l,j) = -clj*P(j)
-          Gmid2(l,j) = -Gnnold(l,j)*conjg(P(j))
-       end do
-    end do
-!   Gmid = Gmid1*Gmid2
-       call zgemm('N','N',m,m,m,zone,&
-       &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
-       &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
-       &zzero,Gmid(1:m,1:m),size(Gmid,dim=1))
-!   Gmid => (1-Gmid)^{-1}
-       Gmid = -Gmid
-    do j = 1, M
-          Gmid(j,j) = 1.+Gmid(j,j)
-    end do
-        call zgetrf( M, M, Gmid, LDAI, IPIV, INFO )
-125     call zgetri( M, Gmid, LDAI, IPIV, WORKI,LWORKI,INFO )
-        if (info .eq. 0) then 
-          if (real(worki(1)) .gt. lworki) then
-            print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
-            lworki = worki(1) 
-            deallocate(worki)
-            allocate(worki(lworki))
-          endif
-        else if (info .eq. -6) then 
-          print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
-          lworki = worki(1) 
-          deallocate(worki)
+     do j = 1, M
+        do l = 1, M
+           clj  = G1(l,j,n+1) ! isolated Green's function of n+1 column
+           G1new(l,j)  = clj
+           !        write(*,*) clj
+           Gmid1(l,j) = -clj*P(j)
+           Gmid2(l,j) = -Gnnold(l,j)*conjg(P(j))
+        end do
+     end do
+     !   Gmid = Gmid1*Gmid2
+     call zgemm('N','N',m,m,m,zone,&
+          &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
+          &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
+          &zzero,Gmid(1:m,1:m),size(Gmid,dim=1))
+     !   Gmid => (1-Gmid)^{-1}
+     Gmid = -Gmid
+     do j = 1, M
+        Gmid(j,j) = 1.+Gmid(j,j)
+     end do
+     call zgetrf( M, M, Gmid, LDAI, IPIV, INFO )
+125  call zgetri( M, Gmid, LDAI, IPIV, WORKI,LWORKI,INFO )
+     if (info .eq. 0) then
+        if (real(worki(1)) .gt. lworki) then
+           print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
+           lworki = worki(1)
+           deallocate(worki)
            allocate(worki(lworki))
-          goto 125
-        else
-          print *, 'cannot continue, info=', info, ' worki(1)=', worki(1)
-          stop
         endif
+     else if (info .eq. -6) then
+        print *, 'lworki adjusted: ',lworki,'->',int(real(worki(1)))
+        lworki = worki(1)
+        deallocate(worki)
+        allocate(worki(lworki))
+        goto 125
+     else
+        print *, 'cannot continue, info=', info, ' worki(1)=', worki(1)
+        stop
+     endif
 
-!   Gnn(n+1)= Gmid*G1(n+1)
-       call zgemm('N','N',m,m,m,zone,&
-       &Gmid(1:m,1:m),size(Gmid,dim=1),&
-       &G1new(1:m,1:m),size(G1new,dim=1),&
-       &zzero,Gnmid(1:m,1:m),size(Gnmid,dim=1))
-    do j = 1, M
-       do l = 1, M
-          Gnn(l,j,n+1)  = Gnmid(l,j) ! Green's function G(n+1,n+1) for n+1 strip
-       end do
-    end do
-    Gnnold = Gnmid
-!   Gn1(n+1)= Gnmid*Vn+1,n*Gn1(n)
-    do j = 1, M
-       do l = 1, M
-          Gmid1(l,j) = -Gnmid(l,j)*P(j)      ! поставила -, потому что Vn+1,n=-P;Vn,n+1=-P*!!!
-          Gmid2(l,j) = -Gnmid(l,j)*conjg(P(l)) !???
-       end do
-    end do
-       call zgemm('N','N',m,m,m,zone,&
-       &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
-       &Gn1old(1:m,1:m),size(Gn1old,dim=1),&  !! Gn1old--->0
-       &zzero,Gnmid(1:m,1:m),size(Gnmid,dim=1))
+     !   Gnn(n+1)= Gmid*G1(n+1)
+     call zgemm('N','N',m,m,m,zone,&
+          &Gmid(1:m,1:m),size(Gmid,dim=1),&
+          &G1new(1:m,1:m),size(G1new,dim=1),&
+          &zzero,Gnmid(1:m,1:m),size(Gnmid,dim=1))
+     do j = 1, M
+        do l = 1, M
+           Gnn(l,j,n+1)  = Gnmid(l,j) ! Green's function G(n+1,n+1) for n+1 strip
+        end do
+     end do
+     Gnnold = Gnmid
+     !   Gn1(n+1)= Gnmid*Vn+1,n*Gn1(n)
+     do j = 1, M
+        do l = 1, M
+           Gmid1(l,j) = -Gnmid(l,j)*P(j)      ! поставила -, потому что Vn+1,n=-P;Vn,n+1=-P*!!!
+           Gmid2(l,j) = -Gnmid(l,j)*conjg(P(l)) !???
+        end do
+     end do
+     call zgemm('N','N',m,m,m,zone,&
+          &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
+          &Gn1old(1:m,1:m),size(Gn1old,dim=1),&  !! Gn1old--->0
+          &zzero,Gnmid(1:m,1:m),size(Gnmid,dim=1))
 
-!   G1n(n+1)= G1n(n)*Vn,n+1*Gnmid
-       call zgemm('N','N',m,m,m,zone,&
-       &G1nold(1:m,1:m),size(G1nold,dim=1),&
-       &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
-       &zzero,Gmid1(1:m,1:m),size(Gmid1,dim=1))
-    do j = 1, M
-       do l = 1, M
-          Gn1(l,j,n+1)  = Gnmid(l,j) ! Green's function G(n+1,1) for n+1 strip 
-          G1n(l,j,n+1)  = Gmid1(l,j) ! Green's function G(1,n+1) for n+1 strip 
-       end do
-    end do
-    Gn1old = Gnmid
-    G1nold = Gmid1
+     !   G1n(n+1)= G1n(n)*Vn,n+1*Gnmid
+     call zgemm('N','N',m,m,m,zone,&
+          &G1nold(1:m,1:m),size(G1nold,dim=1),&
+          &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
+          &zzero,Gmid1(1:m,1:m),size(Gmid1,dim=1))
+     do j = 1, M
+        do l = 1, M
+           Gn1(l,j,n+1)  = Gnmid(l,j) ! Green's function G(n+1,1) for n+1 strip
+           G1n(l,j,n+1)  = Gmid1(l,j) ! Green's function G(1,n+1) for n+1 strip
+        end do
+     end do
+     Gn1old = Gnmid
+     G1nold = Gmid1
   end do
 
   deallocate(Gn1old)
@@ -214,6 +214,6 @@ subroutine calc_GreenL(m,Nx,v,tv,hmag,ef,p,sigmaL,sigmaR,Gnn,Gn1,G1n)
   deallocate(g1i)
   deallocate(EmHi)
   deallocate(WORKI)
-  deallocate(IPIV) 
+  deallocate(IPIV)
 
 end subroutine calc_GreenL
--- mainCONDE.f
+++ mainCONDE.f
@@ -1,25 +1,25 @@
 ! -*- f90 -*-
 program Sheglov
-!  include 'mpif.h'
-!  use mpi
+  !  include 'mpif.h'
+  !  use mpi
   use params
   use potential2d
 
   implicit none
 
   interface
-    subroutine calc_gamma(m,v,tv,hmag,ef,p,sigma,gamma)
-      use params
-      integer, intent(in) :: m      !number of points in v
-      real(WP), intent(in) :: v(m)  !potential in the channel
-      real(WP), intent(in) :: tv    !cell size
-      real(WP), intent(in) :: hmag  !magnetic field
-      real(WP), intent(in) :: ef    !Fermi energy
-      complex(WP), intent(in) :: p(m) !p(k)=exp(2*i*Pi*hmag*k)
-      complex(WP), intent(out) :: gamma(1:m,1:m)
-      complex(WP), intent(out) :: sigma(1:m,1:m)
-    end subroutine calc_gamma
-  end interface 
+     subroutine calc_gamma(m,v,tv,hmag,ef,p,sigma,gamma)
+       use params
+       integer, intent(in) :: m      !number of points in v
+       real(WP), intent(in) :: v(m)  !potential in the channel
+       real(WP), intent(in) :: tv    !cell size
+       real(WP), intent(in) :: hmag  !magnetic field
+       real(WP), intent(in) :: ef    !Fermi energy
+       complex(WP), intent(in) :: p(m) !p(k)=exp(2*i*Pi*hmag*k)
+       complex(WP), intent(out) :: gamma(1:m,1:m)
+       complex(WP), intent(out) :: sigma(1:m,1:m)
+     end subroutine calc_gamma
+  end interface
 
   complex(WP), allocatable :: GLn1(:,:,:), GLnn(:,:,:), GL1n(:,:,:)
   complex(WP), allocatable :: p(:), pc(:)
@@ -38,17 +38,17 @@ program Sheglov
   complex(WP) :: emt, emkt
   real(WP) :: tv
   complex(WP), parameter::zzero=(0.0,0.0)
-  complex(WP), parameter::zone = (1.0,0.0)   
+  complex(WP), parameter::zone = (1.0,0.0)
 
   ! read precomputed 2D potential into array
   call p2d_read(vxy1,'u-new-map.dat')
   write(*,*) vxy1%nx, vxy1%ny, vxy1%hx,vxy1%hy
-!  write(*,*) vxy%nx, vxy%ny, vxy%hx,vxy%hy
-!  vxy%u = (vxy%u+vxy1%u+vxy2%u)/3.
+  !  write(*,*) vxy%nx, vxy%ny, vxy%hx,vxy%hy
+  !  vxy%u = (vxy%u+vxy1%u+vxy2%u)/3.
   !scale from eV to meV
   vxy1%u = vxy1%u * 1000.
   call p2d_write_xyz(vxy1,'test1.dat')
-  iyshift=30!20!20!60!20 
+  iyshift=30!20!20!60!20
   ixshift=0!30!24!10!60!15
   vxy%xmax=vxy1%xmax-ixshift*vxy1%hx
   vxy%ymax=vxy1%ymax-iyshift*vxy1%hy
@@ -60,17 +60,17 @@ program Sheglov
   vxy%hx=vxy1%hx
   call p2d_alloc(vxy,vxy%nx,vxy%ny)
   write(*,*) vxy%nx, vxy%ny, vxy%hx,vxy%hy
-!  call p2d_alloc(vxy,vxy1%nx,vxy1%ny)
+  !  call p2d_alloc(vxy,vxy1%nx,vxy1%ny)
   do i=1,vxy%nx  ! одно сужение, вход
-  do j=1,vxy%ny
-     vxy%u(i,j) = vxy1%u(i+ixshift,j+iyshift)
-  end do
+     do j=1,vxy%ny
+        vxy%u(i,j) = vxy1%u(i+ixshift,j+iyshift)
+     end do
   end do
   call p2d_remesh(vxy,2*vxy%nx-1,2*vxy%ny-1)
   write(*,*) vxy%nx, vxy%ny, vxy%hx,vxy%hy
   call p2d_write_xyz(vxy,'test2.dat')
-!  stop
-5  open(DTOUT,file=DATAFILE,status='replace',iostat=ierr)
+  !  stop
+5 open(DTOUT,file=DATAFILE,status='replace',iostat=ierr)
   write (DTOUT,'(1x,t1,''G 11 3 176'')')
   if(ierr.ne.0) then
      write(*,*) 'Error writing file ',DATAFILE
@@ -115,65 +115,65 @@ program Sheglov
   vL(1:m) = vxy%u(1,1:vxy%ny);
   vR(1:m) = vxy%u(Nx,1:vxy%ny);
 
- do ief=iEfmin,iEfmax
-   Ef=Emin+deltaE*(iEf-1)
-!   Ef0=Emin+deltaE*(iEf-1)
+  do ief=iEfmin,iEfmax
+     Ef=Emin+deltaE*(iEf-1)
+     !   Ef0=Emin+deltaE*(iEf-1)
 15   do ibt=1,ibtmax
-      Btesla=B_min+deltaB*(ibt-1)
-!      Ewc=4.*PI*1.602/6.625*1.0e-4*E00*Btesla
-!      Ef=Ef0!+Ewc
-      hmag = btesla * b**2 * 0.006/25.
-      emt = exp(cmplx(0.0,2.0*PI*hmag,WP))
-      P(1) = emt
-      emkt = emt
-      do k = 2, M-1
-         emkt = emkt * emt
-         P(k) = emkt
-      end do
-      P(M)=emkt*emt
-      Pc=conjg(P)
-!left transmission!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  call calc_gamma(vxy%ny,vL(1:m),tv,hmag,ef,Pc,sigmaL,gammaL)
-  call calc_gamma(vxy%ny,vR(1:m),tv,hmag,ef,P,sigmaR,gammaR)
-  call calc_GreenL(M,Nx,vxy%u(1:Nx,1:M),tv,hmag,ef,P,sigmaL,sigmaR,GLnn,GLn1,GL1n)
-    do j = 1, M
-       do i = 1, M
-          gn1_N(i,j)=GLn1(i,j,Nx) 
-       end do
-    end do
-       call zgemm('N','N',m,m,m,zone,&
-       &gammaR(1:m,1:m),size(gammaR,dim=1),&
-       &gn1_N(1:m,1:m),size(gn1_N,dim=1),&  !???
-       &zzero,Gmid1(1:m,1:m),size(Gmid1,dim=1))
-       gn1_NTs=transpose(gn1_N)
-    do j = 1, M
-       do i = 1, M
-          gn1_N(i,j)=conjg(Gn1_NTs(i,j)) 
-       end do
-    end do
-       call zgemm('N','N',m,m,m,zone,&
-       &gammaL(1:m,1:m),size(gammaL,dim=1),&
-       &gn1_N(1:m,1:m),size(gn1_N,dim=1),&
-       &zzero,Gmid2(1:m,1:m),size(Gmid2,dim=1))
+        Btesla=B_min+deltaB*(ibt-1)
+        !      Ewc=4.*PI*1.602/6.625*1.0e-4*E00*Btesla
+        !      Ef=Ef0!+Ewc
+        hmag = btesla * b**2 * 0.006/25.
+        emt = exp(cmplx(0.0,2.0*PI*hmag,WP))
+        P(1) = emt
+        emkt = emt
+        do k = 2, M-1
+           emkt = emkt * emt
+           P(k) = emkt
+        end do
+        P(M)=emkt*emt
+        Pc=conjg(P)
+        !left transmission!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        call calc_gamma(vxy%ny,vL(1:m),tv,hmag,ef,Pc,sigmaL,gammaL)
+        call calc_gamma(vxy%ny,vR(1:m),tv,hmag,ef,P,sigmaR,gammaR)
+        call calc_GreenL(M,Nx,vxy%u(1:Nx,1:M),tv,hmag,ef,P,sigmaL,sigmaR,GLnn,GLn1,GL1n)
+        do j = 1, M
+           do i = 1, M
+              gn1_N(i,j)=GLn1(i,j,Nx)
+           end do
+        end do
+        call zgemm('N','N',m,m,m,zone,&
+             &gammaR(1:m,1:m),size(gammaR,dim=1),&
+             &gn1_N(1:m,1:m),size(gn1_N,dim=1),&  !???
+             &zzero,Gmid1(1:m,1:m),size(Gmid1,dim=1))
+        gn1_NTs=transpose(gn1_N)
+        do j = 1, M
+           do i = 1, M
+              gn1_N(i,j)=conjg(Gn1_NTs(i,j))
+           end do
+        end do
+        call zgemm('N','N',m,m,m,zone,&
+             &gammaL(1:m,1:m),size(gammaL,dim=1),&
+             &gn1_N(1:m,1:m),size(gn1_N,dim=1),&
+             &zzero,Gmid2(1:m,1:m),size(Gmid2,dim=1))
 
-       call zgemm('N','N',m,m,m,zone,&
-       &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
-       &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
-       &zzero,Gn1_N(1:m,1:m),size(Gn1_N,dim=1))
-    sled=0.
-    do j = 1, M
-          sled=sled+(Gn1_N(j,j))
-    end do
-    if(sled.lt.0) then
-       Ef=Ef+0.001*deltaE
-       goto 15
-    end if
-!    write(DTOUT,99) Ef, sled, Btesla!, grtot+gtot
-   write(DTOUT,98) Btesla, sled, Ef!, grtot+gtot
-  write(*,*) Btesla, sled, Ef
+        call zgemm('N','N',m,m,m,zone,&
+             &Gmid1(1:m,1:m),size(Gmid1,dim=1),&
+             &Gmid2(1:m,1:m),size(Gmid2,dim=1),&
+             &zzero,Gn1_N(1:m,1:m),size(Gn1_N,dim=1))
+        sled=0.
+        do j = 1, M
+           sled=sled+(Gn1_N(j,j))
+        end do
+        if(sled.lt.0) then
+           Ef=Ef+0.001*deltaE
+           goto 15
+        end if
+        !    write(DTOUT,99) Ef, sled, Btesla!, grtot+gtot
+        write(DTOUT,98) Btesla, sled, Ef!, grtot+gtot
+        write(*,*) Btesla, sled, Ef
+     end do
   end do
-end do
-   close(DTOUT)
+  close(DTOUT)
   deallocate(vL)
   deallocate(vR)
   deallocate(gammaL)
@@ -189,7 +189,6 @@ end do
   deallocate(Gmid2)
   deallocate(p)
   stop
-99  format(g13.6,3x,g13.6,3x,g13.6,3x,g13.6)
-98  format(g13.6,3x,g15.8,3x,g13.6)
+99 format(g13.6,3x,g13.6,3x,g13.6,3x,g13.6)
+98 format(g13.6,3x,g15.8,3x,g13.6)
 end program Sheglov
-                         
